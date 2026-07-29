ee48177ef2e56ad61451e5bf0c86cf2415b7d700
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-29 14:41:09 -0300

    refactor: restructure App component and add Header with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-export default function app() {
+import { Header } from "./components/Header";
+import styles from "./app.module.css";
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className={styles.container}>
+      <main>
+        <Header />
+      </main>
     </div>
   );
 }
