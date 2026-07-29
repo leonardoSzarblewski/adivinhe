75a251bba6fb4852b0fdfd126559e3d1825e47d7
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-29 14:55:14 -0300

    feat: add Header component with styles and restart button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
 import logo from "../../assets/logo.png";
+import restart from "../../assets/restart.svg";
 import styles from "./styles.module.css";
 
 export function Header() {
   return (
     <div className={styles.container}>
       <img src={logo} alt="logo" />
+
+      <header>
+        <span>
+          <strong>5</strong> de 10 tentativas
+        </span>
+
+        <button type="button">
+          <img src={restart} alt="ícone de reiniciar" />
+        </button>
+      </header>
     </div>
   );
 }
