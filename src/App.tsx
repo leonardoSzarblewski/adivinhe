bb4f0a26f732e9328c4423e209c72231d54830f3
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-29 15:06:49 -0300

    fix: correct typo in handleRestartGame function name

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Header } from "./components/Header";
 import styles from "./app.module.css";
 
 export default function App() {
-  function hendleRestartGame() {
+  function handleRestartGame() {
     alert("Reiniciar o jogo");
   }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={hendleRestartGame} />
+        <Header current={5} max={10} onRestart={handleRestartGame} />
       </main>
     </div>
   );
