4cc47d5012a2f368dffe7956a7bb71183b98436a
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-29 15:04:06 -0300

    feat: implement game restart functionality in Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { Header } from "./components/Header";
 import styles from "./app.module.css";
 
 export default function App() {
+  function hendleRestartGame() {
+    alert("Reiniciar o jogo");
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={hendleRestartGame} />
       </main>
     </div>
   );
