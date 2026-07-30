27f7468f49ce9748e79a78d19f5b3e7505ac2978
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-30 15:58:00 -0300

    feat: update Letter component to support size prop and add styles for small letters in LettersUsed component

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,15 @@
 import styles from "./styles.module.css";
+import { Letter } from "../Letter";
 
 export function LettersUsed() {
   return (
     <div className={styles.lettesrUsed}>
       <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter value="X" size="small" />
+        <Letter value="R" size="small" />
+      </div>
     </div>
   );
 }
