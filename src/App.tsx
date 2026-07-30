ee02c4f64d85b1ba6620709d178fff09e9c3b810
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-30 15:47:18 -0300

    feat: add LettersUsed component and styles to display used letters in the game

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
+import { LettersUsed } from "./components/LettersUsed";
 import styles from "./app.module.css";
 
 export default function App() {
@@ -15,19 +16,17 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Linguagem de programação mais utilizada no mercado" />
-
         <div className={styles.word}>
           <Letter value="L" />
           <Letter value="E" />
           <Letter value="O" />
         </div>
-
         <h4>Palpite</h4>
-
         <div className={styles.guess}>
           <Input autoFocus maxLength={1} placeholder="?" />
           <Button title="Confirmar" />
         </div>
+        <LettersUsed />
       </main>
     </div>
   );
