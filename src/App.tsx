04776f1a00c86b10e5a778f942c146965b130829
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-30 15:31:33 -0300

    feat: add Button component and styles, update App layout to include button for user confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
+import { Button } from "./components/Button";
 import styles from "./app.module.css";
 
 export default function App() {
@@ -23,8 +24,9 @@ export default function App() {
 
         <h4>Palpite</h4>
 
-        <div>
+        <div className={styles.guess}>
           <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
         </div>
       </main>
     </div>
