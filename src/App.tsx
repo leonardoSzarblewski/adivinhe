6067efaf6f7441206e1a9e62d2da9922d865a56d
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-31 15:59:52 -0300

    fix: update Header component to display current score and max attempts correctly

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ export default function App() {
   const [lettersUsed, setLetterUsed] = useState<LetterUsedProps[]>([]);
   const [challenge, setChallenge] = useState<Challenge | null>(null);
 
+  const ATTEMPTS_MARGIN = 5;
+
   function handleRestartGame() {
     alert("Reiniciar o jogo");
   }
@@ -75,7 +77,11 @@ export default function App() {
   return (
     <div className={styles.container}>
       <main>
-        <Header current={score} max={10} onRestart={handleRestartGame} />
+        <Header
+          current={lettersUsed.length}
+          max={challenge.word.length + ATTEMPTS_MARGIN}
+          onRestart={handleRestartGame}
+        />
 
         <Tip tip={challenge.tip} />
 
