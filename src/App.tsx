303443b6abaea3536cfbeb6bb798e5db9eee0ee8
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-31 14:21:12 -0300

    feat: implement game logic with state management for attempts and challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
 import { LettersUsed } from "./components/LettersUsed";
+
+import { WORDS, type Challenge } from "./utils/words";
+
 import styles from "./app.module.css";
 
 export default function App() {
+  const [attempts, setAttempts] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
   function handleRestartGame() {
     alert("Reiniciar o jogo");
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+    setAttempts(0);
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
+
         <Tip tip="Linguagem de programação mais utilizada no mercado" />
+
         <div className={styles.word}>
           <Letter value="L" />
           <Letter value="E" />
